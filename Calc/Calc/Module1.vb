8c305f04be1df91926aaf98d0141820300dd7ea8
udonko <orange35miyamoto@yahoo.co.jp>
2019-05-29 13:52:29 +0900

    計算追加

---
+++ Calc/Calc/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num, cal As Integer
+        num = Integer.Parse(Console.ReadLine())
+        cal += num
+        Console.WriteLine("＋：{0}", cal)
+    End Sub
+
+End Module
