a67039b1d34289198dc9b1684f18be10dde377f5
udonko <orange35miyamoto@yahoo.co.jp>
2019-05-29 13:39:09 +0900

    名前を入力させ、その名前に対して返答する機能を追加。

--- HelloWorld/HelloWorld/Module1.vb
+++ HelloWorld/HelloWorld/Module1.vb
@@ -1,7 +1,10 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim Name As String
+        Name = Console.ReadLine()
         Console.WriteLine("Hello World!")
+        Console.WriteLine("{0} さん、こんにちは！", Name)
     End Sub
 
 End Module
